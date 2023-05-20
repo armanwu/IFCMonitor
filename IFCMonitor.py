1ec4be172116f9038c5f21606f46da98353f9b64
Arman Arisman <arman.wu@gmail.com>
2023-05-20 14:45:30 +0700

    Update IFCMonitor.py

--- IFCMonitor.py
+++ IFCMonitor.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import filedialog
 import ifcopenshell
+from collections import Counter
 
 def load_ifc_file():
     filepath = filedialog.askopenfilename(filetypes=[("IFC Files", "*.ifc")])
@@ -9,18 +10,32 @@ def load_ifc_file():
         ifc_file = ifcopenshell.open(filepath)
 
         entities = ifc_file.by_type("IfcProduct")
-        for entity in entities:
-            print(entity.is_a())
+        entity_names = [entity.is_a() for entity in entities]
+        entity_counts = Counter(entity_names)
 
- 
+        # Clear the listbox
+        entity_listbox.delete(0, tk.END)
+
+        # Insert entity names and counts into the listbox
+        for name, count in entity_counts.items():
+            display_text = f"{name} ({count})"
+            entity_listbox.insert(tk.END, display_text)
+
+
 def main():
+    global entity_listbox
+
     root = tk.Tk()
     root.title("IFC Monitor")
 
     load_button = tk.Button(root, text="Load IFC", command=load_ifc_file)
     load_button.pack()
 
+    entity_listbox = tk.Listbox(root, height=20, width=50)
+    entity_listbox.pack()
+
     root.mainloop()
+
 
 if __name__ == "__main__":
     main()
